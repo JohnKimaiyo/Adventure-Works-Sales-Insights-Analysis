1b9bf41b7380f55b2d04e286ff6d14d6e502d2ab
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-07-02 11:35:35 +0300

    final commit

---
+++ fullname.sql
@@ -0,0 +1,6 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT CONCAT(FirstName,'  ',LastName,'  ',MiddleName  )
+      
+    
+
+  FROM [AdventureWorksDW2019].[dbo].[DimEmployee]
