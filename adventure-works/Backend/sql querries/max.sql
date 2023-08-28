d12646382ae17ead49b0e7e1e295cbc8029c7097
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-28 18:48:43 +0300

    final commit

--- adventure-works/Backend/sql querries/max.sql
+++ adventure-works/Backend/sql querries/max.sql
@@ -1,2 +1,6 @@
 /****** Script for SelectTopNRows command from SSMS  ******/
 SELECT max([Rate]) FROM [AdventureWorks2016].[HumanResources].[EmployeePayHistory]
+
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT MAX([StandardCost]) 
+  FROM [AdventureWorks2016].[Production].[ProductCostHistory]
