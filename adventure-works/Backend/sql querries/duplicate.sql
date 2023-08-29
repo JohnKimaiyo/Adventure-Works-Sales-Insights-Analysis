3072a132111767d4a4abc50ed8ed884d4da4e9d7
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-29 10:33:33 +0300

    final commit

--- adventure-works/Backend/sql querries/duplicate.sql
+++ adventure-works/Backend/sql querries/duplicate.sql
@@ -4,3 +4,8 @@ SELECT [JobTitle]  ,count(*)
   FROM [AdventureWorks2016].[HumanResources].[Employee]
   group by [JobTitle]
   
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT [Name],count(*)
+  FROM [AdventureWorks2016].[Production].[Product]
+  group by [Name]
