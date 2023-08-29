f2ca1e849f049ee2510cff3f5ca12974ff5c7c80
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-29 13:19:24 +0300

    like

--- adventure-works/Backend/sql querries/like.sql
+++ adventure-works/Backend/sql querries/like.sql
@@ -130,3 +130,15 @@ SELECT TOP (1000) [BusinessEntityID]
       ,[ModifiedDate]
   FROM [AdventureWorks2016].[Person].[Person]
   WHERE [LastName] like '___';
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [StateProvinceID]
+      ,[StateProvinceCode]
+      ,[CountryRegionCode]
+      ,[IsOnlyStateProvinceFlag]
+      ,[Name]
+      ,[TerritoryID]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2016].[Person].[StateProvince]
+  WHERE [Name] like '___';
