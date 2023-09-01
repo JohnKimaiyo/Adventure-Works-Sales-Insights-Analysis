723694c4a77120340c434b98b2feeaa224c773ac
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-09-01 16:23:45 +0300

    where

--- adventure-works/Basics/filter records/where clause/project 8/where clause.sql
+++ adventure-works/Basics/filter records/where clause/project 8/where clause.sql
@@ -17,3 +17,23 @@ SELECT TOP (1000) [BusinessEntityID]
       ,[ModifiedDate]
   FROM [AdventureWorks2016].[HumanResources].[Employee]
   WHERE [MaritalStatus] = 's';
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [BusinessEntityID]
+      ,[NationalIDNumber]
+      ,[LoginID]
+      ,[OrganizationNode]
+      ,[OrganizationLevel]
+      ,[JobTitle]
+      ,[BirthDate]
+      ,[MaritalStatus]
+      ,[Gender]
+      ,[HireDate]
+      ,[SalariedFlag]
+      ,[VacationHours]
+      ,[SickLeaveHours]
+      ,[CurrentFlag]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2016].[HumanResources].[Employee]
+  WHERE (year)= '1985';
