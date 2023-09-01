6121cdef8535b3caa18f5fb021fb42afb00dcb4b
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-09-01 17:07:47 +0300

    finla

---
+++ adventure-works/Basics/filter records/comparison operators/project 7/Operator.sql
@@ -0,0 +1,11 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [StateProvinceID]
+      ,[StateProvinceCode]
+      ,[CountryRegionCode]
+      ,[IsOnlyStateProvinceFlag]
+      ,[Name]
+      ,[TerritoryID]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2016].[Person].[StateProvince]
+  WHERE CountryRegionCode <> 'US';
