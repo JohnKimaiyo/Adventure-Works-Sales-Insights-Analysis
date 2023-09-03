9d9e26361f26b9c39b72488befbeb6f4304aa0a1
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-09-03 15:44:31 +0300

    operstor

--- adventure-works/Basics/filter records/comparison operators/project 20/Operator.sql
+++ adventure-works/Basics/filter records/comparison operators/project 20/Operator.sql
@@ -21,3 +21,38 @@ SELECT TOP (1000) [StateProvinceID]
       ,[ModifiedDate]
   FROM [AdventureWorks2016].[Person].[StateProvince]
   WHERE CountryRegionCode != 'US';
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [StateProvinceID]
+      ,[StateProvinceCode]
+      ,[CountryRegionCode]
+      ,[IsOnlyStateProvinceFlag]
+      ,[Name]
+      ,[TerritoryID]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2016].[Person].[StateProvince]
+  WHERE CountryRegionCode <> 'US';
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [StateProvinceID]
+      ,[StateProvinceCode]
+      ,[CountryRegionCode]
+      ,[IsOnlyStateProvinceFlag]
+      ,[Name]
+      ,[TerritoryID]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2016].[Person].[StateProvince]
+  WHERE CountryRegionCode !='US';
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [StateProvinceID]
+      ,[StateProvinceCode]
+      ,[CountryRegionCode]
+      ,[IsOnlyStateProvinceFlag]
+      ,[Name]
+      ,[TerritoryID]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2016].[Person].[StateProvince]
+  WHERE StateProvinceCode<>CountryRegionCode
