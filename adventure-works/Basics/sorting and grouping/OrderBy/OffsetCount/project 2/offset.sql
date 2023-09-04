e8620486cf2cc80092957f4c94eef1bab2d12dbe
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-09-04 12:09:44 +0300

    offset

---
+++ adventure-works/Basics/sorting and grouping/OrderBy/OffsetCount/project 2/offset.sql
@@ -0,0 +1,20 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT [BusinessEntityID]
+      ,[NationalIDNumber]
+      ,[LoginID]
+      ,[OrganizationNode]
+      ,[OrganizationLevel]
+      ,[JobTitle]
+      ,[BirthDate]
+      ,[MaritalStatus]
+      ,[Gender]
+      ,[HireDate]
+      ,[SalariedFlag]
+      ,[VacationHours]
+      ,[SickLeaveHours]
+      ,[CurrentFlag]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2016].[HumanResources].[Employee]
+  ORDER BY BusinessEntityID ASC
+  OFFSET 5 ROW;
