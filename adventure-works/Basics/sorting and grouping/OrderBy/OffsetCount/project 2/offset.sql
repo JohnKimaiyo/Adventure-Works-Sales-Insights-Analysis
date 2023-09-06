8fc9bb434d59a439f53431d17bb2b44aceea64a4
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-09-06 14:01:41 +0300

    fetch

--- adventure-works/Basics/sorting and grouping/OrderBy/OffsetCount/project 2/offset.sql
+++ adventure-works/Basics/sorting and grouping/OrderBy/OffsetCount/project 2/offset.sql
@@ -28,3 +28,25 @@ SELECT  [DepartmentID]
   ORDER BY DepartmentID ASC
   OFFSET 5 ROWS
   FETCH NEXT 10 ROWS ONLY;
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT [BusinessEntityID]
+      ,[NationalIDNumber]
+      ,[LoginID]
+      ,[OrganizationNode]
+      ,[OrganizationLevel]
+      ,[JobTitle]
+      ,[BirthDate]
+      ,[MaritalStatus]
+      ,[Gender]
+      ,[HireDate]
+      ,[SalariedFlag]
+      ,[VacationHours]
+      ,[SickLeaveHours]
+      ,[CurrentFlag]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2016].[HumanResources].[Employee]
+  ORDER BY ModifiedDate DESC
+  OFFSET 0 ROWS
+  FETCH  NEXT 5 ROWS ONLY;
