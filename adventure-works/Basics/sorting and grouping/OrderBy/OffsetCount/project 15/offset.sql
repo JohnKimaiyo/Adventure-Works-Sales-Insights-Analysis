cec78e75eb803510c6091fe421b9d29fb9e5de33
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-09-06 13:08:02 +0300

    fetch

--- adventure-works/Basics/sorting and grouping/OrderBy/OffsetCount/project 15/offset.sql
+++ adventure-works/Basics/sorting and grouping/OrderBy/OffsetCount/project 15/offset.sql
@@ -5,3 +5,17 @@ SELECT  [ProductID]
   FROM [AdventureWorks2016].[Production].[ProductDocument]
   ORDER BY ProductID ASC
   OFFSET 10 ROW;
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT  [StateProvinceID]
+      ,[StateProvinceCode]
+      ,[CountryRegionCode]
+      ,[IsOnlyStateProvinceFlag]
+      ,[Name]
+      ,[TerritoryID]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2016].[Person].[StateProvince]
+  ORDER BY StateProvinceID ASC
+  OFFSET 10 ROWS
+  FETCH NEXT 10 ROWS ONLY;
