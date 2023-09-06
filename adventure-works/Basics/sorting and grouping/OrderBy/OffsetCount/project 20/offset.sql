08b31bf62a65f9665ba34209c031800e94a03b8e
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-09-06 17:32:52 +0300

    fetch

--- adventure-works/Basics/sorting and grouping/OrderBy/OffsetCount/project 20/offset.sql
+++ adventure-works/Basics/sorting and grouping/OrderBy/OffsetCount/project 20/offset.sql
@@ -19,3 +19,14 @@ SELECT [ProductID]
   ORDER BY Quantity ASC
   OFFSET 10 ROWS
   FETCH NEXT 10 ROWS ONLY;
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT [ProductID]
+      ,[StartDate]
+      ,[EndDate]
+      ,[StandardCost]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2016].[Production].[ProductCostHistory]
+  ORDER BY StandardCost DESC
+  OFFSET 0 ROWS
+  FETCH NEXT 10 ROWS ONLY;
