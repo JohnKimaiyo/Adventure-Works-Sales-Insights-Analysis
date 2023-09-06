3b90dda623f257d4e04e747f6ce5b5f4bc53ebf2
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-09-06 12:18:00 +0300

    fetch

--- adventure-works/Basics/sorting and grouping/OrderBy/OffsetCount/project 5/offset.sql
+++ adventure-works/Basics/sorting and grouping/OrderBy/OffsetCount/project 5/offset.sql
@@ -5,3 +5,13 @@ SELECT  [BusinessEntityID]
   FROM [AdventureWorks2016].[Person].[BusinessEntity]
   ORDER BY ModifiedDate ASC
   OFFSET 10 ROW;
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT  [BusinessEntityID]
+      ,[RateChangeDate]
+      ,[Rate]
+      ,[PayFrequency]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2016].[HumanResources].[EmployeePayHistory]
+  ORDER BY BusinessEntityID ASC
+  OFFSET 10 ROWS
+  FETCH NEXT 10 ROWS ONLY;
