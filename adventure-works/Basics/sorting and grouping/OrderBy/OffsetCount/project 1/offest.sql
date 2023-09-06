a94673ea270e4a7d97065b154cac979c72be2851
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-09-06 12:04:59 +0300

    fetch

--- adventure-works/Basics/sorting and grouping/OrderBy/OffsetCount/project 1/offest.sql
+++ adventure-works/Basics/sorting and grouping/OrderBy/OffsetCount/project 1/offest.sql
@@ -7,3 +7,15 @@ SELECT [DepartmentID]
 
   ORDER BY [DepartmentID] ASC
   OFFSET 5 ROW;
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT [DepartmentID]
+      ,[Name]
+      ,[GroupName]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2016].[HumanResources].[Department]
+
+  ORDER BY [DepartmentID] ASC
+  OFFSET 5 ROW
+  FETCH NEXT 10 ROWS ONLY;
+
