92a5b57a4e92be3b1da03813c53c1d01e9fa171a
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-09-06 16:43:52 +0300

    fetch

--- adventure-works/Basics/sorting and grouping/OrderBy/OffsetCount/project 11/offset.sql
+++ adventure-works/Basics/sorting and grouping/OrderBy/OffsetCount/project 11/offset.sql
@@ -15,3 +15,14 @@ SELECT [BusinessEntityID]
   FROM [AdventureWorks2016].[Person].[Person]
   ORDER BY BusinessEntityID ASC
   OFFSET 10 ROW;
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT [BusinessEntityID]
+      ,[EmailAddressID]
+      ,[EmailAddress]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2016].[Person].[EmailAddress]
+  ORDER BY ModifiedDate DESC
+  OFFSET 0 ROWS
+  FETCH NEXT 5 ROWS ONLY;
