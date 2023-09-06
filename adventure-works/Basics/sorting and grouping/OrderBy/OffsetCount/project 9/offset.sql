1b6f8c539a19e54a4b34398cd167e9e3855a7ac5
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-09-06 12:52:10 +0300

    fetch

--- adventure-works/Basics/sorting and grouping/OrderBy/OffsetCount/project 9/offset.sql
+++ adventure-works/Basics/sorting and grouping/OrderBy/OffsetCount/project 9/offset.sql
@@ -5,3 +5,12 @@ SELECT [CountryRegionCode]
   FROM [AdventureWorks2016].[Person].[CountryRegion]
   ORDER BY Name ASC
   OFFSET 10 ROW;
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT [ContactTypeID]
+      ,[Name]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2016].[Person].[ContactType]
+  ORDER BY ContactTypeID ASC
+  OFFSET 10 ROWS
+  FETCH NEXT 10 ROWS ONLY;
