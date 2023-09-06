fb6b65b367bd3108974909e925dba904f201c16d
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-09-06 13:11:37 +0300

    fetch

--- adventure-works/Basics/sorting and grouping/OrderBy/OffsetCount/project 16/offset.sql
+++ adventure-works/Basics/sorting and grouping/OrderBy/OffsetCount/project 16/offset.sql
@@ -9,3 +9,18 @@ SELECT  [ProductID]
   FROM [AdventureWorks2016].[Production].[ProductInventory]
   ORDER BY ModifiedDate 
   OFFSET 10 ROW;
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT  [BillOfMaterialsID]
+      ,[ProductAssemblyID]
+      ,[ComponentID]
+      ,[StartDate]
+      ,[EndDate]
+      ,[UnitMeasureCode]
+      ,[BOMLevel]
+      ,[PerAssemblyQty]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2016].[Production].[BillOfMaterials]
+  ORDER BY ComponentID ASC
+  OFFSET 10 ROWS
+  FETCH NEXT 10 ROWS ONLY;
