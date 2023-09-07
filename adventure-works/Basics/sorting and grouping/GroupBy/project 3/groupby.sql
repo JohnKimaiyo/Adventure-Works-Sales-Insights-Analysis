6c8ce81fd0f08528ed4dd75eb65d7dea932d2308
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-09-07 14:32:30 +0300

    groupby

--- adventure-works/Basics/sorting and grouping/GroupBy/project 3/groupby.sql
+++ adventure-works/Basics/sorting and grouping/GroupBy/project 3/groupby.sql
@@ -3,3 +3,8 @@ SELECT CountryRegionCode,COUNT
 ( CountryRegionCode) 
   FROM [AdventureWorks2016].[Person].[StateProvince]
   GROUP BY  CountryRegionCode;
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT CostRate, SUM(CostRate)  AS TotalCostRate
+  FROM [AdventureWorks2016].[Production].[Location]
+  GROUP BY CostRate;
