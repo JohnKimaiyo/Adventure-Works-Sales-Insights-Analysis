51d93666a5ea9db00431792ddbdf2a6f99d79d10
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-09-07 14:18:40 +0300

    groupby

--- adventure-works/Basics/sorting and grouping/GroupBy/project 2/groupby.sql
+++ adventure-works/Basics/sorting and grouping/GroupBy/project 2/groupby.sql
@@ -3,3 +3,8 @@ SELECT PersonType,COUNT([PersonType])[BusinessEntityID]
      
   FROM [AdventureWorks2016].[Person].[Person]
   GROUP BY PersonType
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT Rate ,SUM(Rate) 
+  FROM [AdventureWorks2016].[HumanResources].[EmployeePayHistory]
+  GROUP BY Rate
