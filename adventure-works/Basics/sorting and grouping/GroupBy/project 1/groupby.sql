51e5f09e041a2ff5ee06ddf2840683a507e57ab7
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-09-07 14:17:17 +0300

    groupby

--- adventure-works/Basics/sorting and grouping/GroupBy/project 1/groupby.sql
+++ adventure-works/Basics/sorting and grouping/GroupBy/project 1/groupby.sql
@@ -3,3 +3,9 @@ SELECT [City],COUNT ([City])
    
   FROM [AdventureWorks2016].[Person].[Address]
   GROUP BY City
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT VacationHours, SUM( VacationHours) as TotalVacantHours
+     
+  FROM [AdventureWorks2016].[HumanResources].[Employee]
+  GROUP BY  VacationHours;
