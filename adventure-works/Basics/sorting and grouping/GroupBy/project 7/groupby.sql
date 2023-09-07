19ffedaba891e27319a1468f37f8f114094c1a3c
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-09-07 14:50:30 +0300

    groupby

--- adventure-works/Basics/sorting and grouping/GroupBy/project 7/groupby.sql
+++ adventure-works/Basics/sorting and grouping/GroupBy/project 7/groupby.sql
@@ -2,3 +2,9 @@
 SELECT CardType, COUNT (CardType) 
   FROM [AdventureWorks2016].[Sales].[CreditCard]
   GROUP BY CardType
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT StandardPrice,SUM (StandardPrice) AS TOTALSTANDARDPRICE
+      
+  FROM [AdventureWorks2016].[Purchasing].[ProductVendor]
+  GROUP BY StandardPrice;
