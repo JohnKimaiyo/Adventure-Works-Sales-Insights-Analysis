be22a1acc6a0a2f9a2be8b080c5377cbe5986e73
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-09-07 14:47:51 +0300

    groupby

--- adventure-works/Basics/sorting and grouping/GroupBy/project 6/groupby.sql
+++ adventure-works/Basics/sorting and grouping/GroupBy/project 6/groupby.sql
@@ -2,3 +2,9 @@
 SELECT CurrencyCode, COUNT(CurrencyCode) 
   FROM [AdventureWorks2016].[Sales].[CountryRegionCurrency]
   GROUP BY CurrencyCode
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT ActualCost ,SUM(ActualCost) AS TotalActualCost
+
+  FROM [AdventureWorks2016].[Production].[TransactionHistoryArchive]
+  GROUP BY ActualCost;
