c1bdbbac551b41221585fa42aea77d47e2332cc9
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-09-07 14:43:33 +0300

    groupby

--- adventure-works/Basics/sorting and grouping/GroupBy/project 5/grouby.sql
+++ adventure-works/Basics/sorting and grouping/GroupBy/project 5/grouby.sql
@@ -3,3 +3,8 @@ SELECT ToCurrencyCode ,COUNT (ToCurrencyCode) [CurrencyRateID]
      
   FROM [AdventureWorks2016].[Sales].[CurrencyRate]
   GROUP BY ToCurrencyCode;
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT ListPrice ,SUM(ListPrice)  AS TOTALLSITPRICE
+     
+  FROM [AdventureWorks2016].[Production].[ProductListPriceHistory]
+  GROUP BY ListPrice;
