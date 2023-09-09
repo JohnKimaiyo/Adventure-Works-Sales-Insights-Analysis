d2534249413e61ac350d254e59fd215ac4b51daf
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-09-09 16:07:34 +0300

    year

--- adventure-works/Basics/functions/date/extract date/day.sql
+++ adventure-works/Basics/functions/date/extract date/day.sql
@@ -5,3 +5,23 @@ SELECT   [DepartmentID]
       ,[ModifiedDate]
 	  ,day(ModifiedDate) AS ExtractDay
   FROM [AdventureWorks2016].[HumanResources].[Department]
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT  [BusinessEntityID]
+      ,[NationalIDNumber]
+      ,[LoginID]
+      ,[OrganizationNode]
+      ,[OrganizationLevel]
+      ,[JobTitle]
+      ,[BirthDate]
+      ,[MaritalStatus]
+      ,[Gender]
+      ,[HireDate]
+      ,[SalariedFlag]
+      ,[VacationHours]
+      ,[SickLeaveHours]
+      ,[CurrentFlag]
+      ,[rowguid]
+      ,[ModifiedDate]
+	  , year(ModifiedDate) AS ExtractYear
+  FROM [AdventureWorks2016].[HumanResources].[Employee]
