d0900dbcbd1b790f2fb981cfaff91c3c222beea7
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-09-09 15:06:02 +0300

    rate

--- adventure-works/Basics/functions/less and numerical function/floor.sql
+++ adventure-works/Basics/functions/less and numerical function/floor.sql
@@ -2,3 +2,10 @@
 SELECT Rate,FLOOR(Rate) 
   FROM [AdventureWorks2016].[HumanResources].[EmployeePayHistory]
   WHERE Rate>0;
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+    
+SELECT Rate,FLOOR(Rate)
+     
+  FROM [AdventureWorks2016].[HumanResources].[EmployeePayHistory]
+  WHERE Rate > 0;
