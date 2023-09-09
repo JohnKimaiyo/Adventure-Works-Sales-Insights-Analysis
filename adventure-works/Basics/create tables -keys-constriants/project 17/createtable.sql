6edc19b6ad6272d3cfb90cc748c0c2018a68c588
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-09-09 19:15:41 +0300

    table

---
+++ adventure-works/Basics/create tables -keys-constriants/project 17/createtable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE stores
+(
+StoreId INT IDENTITY,
+StoreNumber Varchar(50),
+PhoneNumber CHAR(14),
+Email Varchar(50),
+Address Varchar(120),
+City Varchar(40),
+State Varchar(10),
+ZipCode INT
+)
