afe6a8176f79e0e725db3ad9b0537642cb69f1a0
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-09-11 13:02:10 +0300

    count:

--- adventure-works/Basics/functions/having/project 1/having.sql
+++ adventure-works/Basics/functions/having/project 1/having.sql
@@ -1,3 +1,10 @@
 /****** Script for SelectTopNRows command from SSMS  ******/
 SELECT max(StandardCost)
   FROM [AdventureWorks2016].[Production].[Product]
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT COUNT (VacationHours) AS TotalNumbeofhours ,JobTitle
+     
+  FROM [AdventureWorks2016].[HumanResources].[Employee]
+  GROUP BY JobTitle
+  HAVING COUNT(VacationHours) >5;
